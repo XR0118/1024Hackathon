76c994fed223f30702b4f4253d90022a0395e967
薛瑞 <xr94118@163.com>
2025-10-25 14:35:14 +0800

    feat: executor init

---
+++ internal/services/master/service/deploy_executor_service.go
@@ -0,0 +1,32 @@
+package service
+
+import (
+	"context"
+
+	"github.com/boreas/internal/interfaces"
+	"github.com/boreas/internal/pkg/models"
+)
+
+type DeployExecutor interface {
+	Apply(ctx context.Context) error
+	UnDo(ctx context.Context) error
+}
+
+type DeployClient interface {
+	Apply(ctx context.Context, app string, version string, pkg models.DeploymentPackage) (models.ApplyResponse, error)
+	AppStatus(ctx context.Context, app string) ([]models.AgentAppStatus, error)
+}
+
+type SimpleDeployExecutor struct {
+	task           models.Task
+	deploymentRepo interfaces.DeploymentRepository
+	client         DeployClient
+}
+
+func NewSimpleDeployExecutor(task models.Task, deploymentRepo interfaces.DeploymentRepository, client DeployClient) *SimpleDeployExecutor {
+	return &SimpleDeployExecutor{
+		task:           task,
+		deploymentRepo: deploymentRepo,
+		client:         client,
+	}
+}
